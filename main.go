2b65fb965f8dd8f700fa10d02117238f82982924
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-23 01:44:26 +0800

    refactor godotenv

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/amirfakhrullah/go-bitly/model"
 	"github.com/amirfakhrullah/go-bitly/server"
-	"github.com/joho/godotenv"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	dsn := os.Getenv("DB_URL_STRING")
 	model.SetupDB(dsn)
 	server.SetupAndListen()
